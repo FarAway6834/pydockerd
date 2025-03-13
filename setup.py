c9525966de23f9508de86a8540837efa355481ef
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-13 11:02:09 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pydockerd',
+    version='0.0.1',
+    description='dockerd but python, on linux, osx, windows, arm, freebsd, solaris',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/pydockerd',
+    packages=find_packages(exclude=[])
+    include_package_data=True,
+    package_data={
+        "mydockerd": ["__main__.pyz"]
+    },
+    keywords=['dockerd', 'docker', 'pydocker', 'pydockerd'],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
