00bf4d569ec2d4572b23753ee71954f92c9d0c40
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-14 09:02:57 +0900

    setting_binarry.py

--- setting_binarry.py
+++ setting_binarry.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryDirectory as tempdir
 from os.path import join as _j
 from urllib.request import urlopen as _o
 from re import search as _s
+from os import rename as __mv__
 
 def __wgetv__(url):
     with _o(url) as res:
@@ -16,8 +17,12 @@ def __wgetv__(url):
 
 def wget(dir, url):
     v = [x for x in __wgetv__(url)]
-    with open(_j(dir, v[1]), 'wb') as fp: fp.write(v[0])
+    fn = _j(dir, v[1])
+    with open(, 'wb') as fp: fp.write(v[0])
+    return fn
 
 with tempdir() as pwd:
     from i, j in getJ('os.json').items():
-        wget(pwd, j)
+        f = wget(pwd, j)
+        tool[i](f)
+        __mv__(f, i)
